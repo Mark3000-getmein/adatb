9a2e5b61870fbdce6c23324ca5d2f5970484566a
Mark3000-getmein <72598891360@szily.hu>
2025-03-25 08:36:59 +0100

    tábla létrehozás teszt elött

--- tablaletrehozasa.php
+++ tablaletrehozasa.php
@@ -1,12 +1,12 @@
 <?php
-/belépéshez szükséges adatokat változókba tesszük
+//belépéshez szükséges adatokat változókba tesszük
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "enABn";
+$dbname = "enabm";
 
 //készíteni kell egy csatlakozást
-$conn=new mysli($servername, $username, $password, $dbname);
+$conn=new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error)
 {
     //hiba esetén lelőjük a csatlakozást
@@ -15,3 +15,31 @@ if($conn->connect_error)
 else{
     echo "Csatlakozás megtörtént";
 }
+
+//tábla létrehozása
+$sql="DROP TABLE IF EXISTS emberek";
+if($conn->query($sql) === true)
+{
+    echo "Sikeres táblatörlés";
+}
+else{
+    echo "Nem sikerült a táblatörlés";
+}
+$sql="";
+$sql = "CREATE TABLE IF NOT EXISTS emberek(
+    id INT unsigned AUTO_INCREMENT PRIMARY KEY,
+    vezeteknev VARCHAR(30) NOT NULL,
+    keresztnev VARCHAR(30) NOT NULL,
+    beosztas VARCHAR(50) NOT NULL,
+    regiszt_datum TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )";
+if($conn->query($sql) === true)
+{
+    echo "Sikeres létrehozás";
+}
+else{
+    echo "Nem sikerült a tábla létrehozás";
+}
+
+//lezárni az adatbázis kapcsolatot, erőforrást felszabadítunk
+$conn->close();
